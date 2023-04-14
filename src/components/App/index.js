14b0742dd35dd63581d593cc2d924264187a87e9
Vicky <vicky.millot@gmail.com>
2023-04-14 09:57:43 +0200

    adding faq page

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,18 @@
 import './styles.scss';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../HomePage';
+import Faq from '../WebsiteInfos/faq';
+import Confidentiality from '../WebsiteInfos/confidentiality';
+import Contact from '../WebsiteInfos/contact';
 
 function App() {
   return (
     <div className="wrapper">
       <Routes>
         <Route path="/accueil" element={<HomePage />} />
+        <Route path="/faq" element={<Faq />} />
+        <Route path="/termes-de-confidentialite" element={<Confidentiality />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
