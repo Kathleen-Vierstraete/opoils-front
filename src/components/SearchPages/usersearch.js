e571c8f1b92abec2dc3eb17f1dd5f663ad0ee228
Vicky <vicky.millot@gmail.com>
2023-04-18 20:59:49 +0200

    working searchfilter functionality, but need to rework later on filteredProfiles const

--- src/components/SearchPages/usersearch.js
+++ src/components/SearchPages/usersearch.js
@@ -1,5 +1,5 @@
 import './styles.scss';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { FETCH_PROFILES } from '../../actions/profiles';
@@ -10,28 +10,45 @@ import AppFooter from '../AppFooter';
 
 const SearchUser = ({ profiles }) => {
   const dispatch = useDispatch();
+  const [searchInput, setSearchInput] = useState('');
 
   useEffect(() => {
     dispatch({ type: FETCH_PROFILES });
-  }, [dispatch]);
+  }, []);
 
+  /* I used Object here because it's global and it takes all the content of the fetched array but I don't think global is a good thing for security a little bit like var instead of let and const but it helps to get results from profiles array instead of checking index by index, I added toLowerCase so uppercase doesn't mess up the filter, without this the filter wouldn't find the right words if there's an uppercase mismatch*/
+
+  /* https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Object */
+  /* https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Array/some */
+
+  const filteredProfiles = profiles.filter((profile) =>
+    Object.values(profile).some(
+      (value) =>
+        typeof value === 'string' &&
+        value.toLowerCase().includes(searchInput.toLowerCase()),
+    ));
   return (
     <>
       <AppHeader />
       <div id="user-search-page">
         <div className="search-div">
           <div id="user-searchbar">
-            <input type="text" placeholder="Rechercher" />
+          <input
+              type="text"
+              placeholder="Rechercher"
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+            />
             <a href="#">
-            <img src={search} alt="search-image" />
+              <img src={search} alt="search-image" />
             </a>
           </div>
-          {profiles && (
-          <div className="cards">
-            {profiles.map((profile) => (
-              <Card key={profile.id} {...profile} />
-            ))}
-          </div>
+          {filteredProfiles && (
+            <div className="cards">
+              {filteredProfiles.map((profile) => (
+                <Card key={profile.id} {...profile} />
+              ))}
+            </div>
           )}
         </div>
       </div>
@@ -40,13 +57,18 @@ const SearchUser = ({ profiles }) => {
   );
 };
 
+/* added here the word prop for the filter, I only need this with Object but if I get rid of it I should remember to add what's needed here */
+
 SearchUser.propTypes = {
   profiles: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
+      word: PropTypes.string.isRequired,
     }),
   ),
 };
+
+/* avoid some errors by messing around while working, profiles is never empty this way it doesn't need any value to work*/
 
 SearchUser.defaultProps = {
   profiles: null,
